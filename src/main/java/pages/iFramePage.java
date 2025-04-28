1e9c72b01e4521d58b5dbd055364495289147225
HoangTH16.DLS <hoangth16.dls@CPP00308800E.fsoft.fpt.vn>
2025-04-28 12:03:08 -0700

    Implement TinyMCE iframe text input automation

---
+++ src/main/java/pages/iFramePage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import core.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utils.PropertiesReader;
+
+import java.io.IOException;
+
+public class iFramePage extends BasePage {
+    private PropertiesReader locators;
+    private By iframeTinyMCE;
+    private By bodyEditor;
+
+    public iFramePage(WebDriver driver) throws IOException {
+        super(driver);
+
+        locators = new PropertiesReader("src/main/resources/locators.properties");
+        iframeTinyMCE = By.id(locators.get("iframe.iframetiny.id"));
+        bodyEditor = By.id(locators.get("iframe.editBody.id"));
+    }
+
+    public void inputTextToTinyMCE(String text) {
+        WebElement iframe = waitForElementVisible(getDriver(), iframeTinyMCE);
+        driver.switchTo().frame(iframe);
+
+        WebElement inputElement = waitForElementVisible(getDriver(), bodyEditor);
+        inputElement.clear();
+        inputElement.sendKeys(text);
+
+        getDriver().switchTo().defaultContent();
+    }
+}
+
---
+++ src/main/java/tests/TC15.java
@@ -0,0 +1,25 @@
+package tests;
+
+import core.BaseTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.JQueryUI;
+import pages.iFramePage;
+import utils.ExcelUtils;
+
+import java.io.IOException;
+
+public class TC15 extends BaseTest {
+
+    @Test(dataProvider = "inputData")
+    public void test(String inputText) throws IOException {
+        iFramePage iframe = new iFramePage(getDriver());
+        iframe.navigateToPage("https://www.tiny.cloud/docs/tinymce/latest/basic-example/");
+        iframe.inputTextToTinyMCE(inputText);
+    }
+
+    @DataProvider(name = "inputData")
+    public Object[][] getAlertData() throws IOException {
+        return ExcelUtils.getTableArray("src/iFrame_data.xlsx", "Sheet1", 0, 1);
+    }
+}
